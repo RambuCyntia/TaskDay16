31b702bef81644c4fbc96f2c7665af706d0e1d9e
UmbuReinhard <reinhardumbu66@gmail.com>
2023-05-31 00:05:12 +0700

    4 Commit

--- namafile.py
+++ namafile.py
@@ -10,19 +10,10 @@ class TestLogin(unittest.TestCase): # test scenario
     def setUp(self):
         self.browser = webdriver.Chrome(ChromeDriverManager().install())
 
-    def test_failed_login(self): #test cases 1
-        driver = self.browser
-        driver.implicitly_wait(10)
-        driver.get("https://www.saucedemo.com/")
-        driver.find_element(By.ID, "user-name").send_keys("haitest")
-        driver.find_element(By.NAME, "login-button").click()
-        error_message = driver.find_element(By.CSS_SELECTOR, "[data-test='error']").text
-        self.assertIn("Epic sadface: Password is required", error_message)
-
     def test_success_login(self): #test cases 2
         driver = self.browser
-        driver.get("https://www.saucedemo.com/")
-        driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        driver.get("https://kasirdemo.belajarqa.com/")
+        driver.find_element(By.ID, "email").send_keys("standard_user")
         driver.find_element(By.CSS_SELECTOR, "[data-test='password']").send_keys("secret_sauce")
         driver.find_element(By.NAME, "login-button").click()
 
